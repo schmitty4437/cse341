61fa5dc37748fb60915b1e5a507c19f8b5d5b45f
schmitty4437 <schmitty4437@gmail.com>
2025-05-15 21:20:28 -0600

    w04 updates

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { connectDb } = require('./db/connection');
 const nameRouter = require('./routes/index');
-const contactsRouter = require('./routes/contacts');
+// const contactsRouter = require('./routes/contacts');
 
 // Enable CORS. Allows requests from any origin. Allows resources on my server to be requested.
 //allow all origins, allow post/get, allow content-type
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Controll-Allow-Methods', 'Content-Type');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
@@ -21,7 +25,7 @@ app.use(express.json());
 
 app.use('/', nameRouter);
 // sends requests with /contacts to routes/contacts.js
-app.use('/contacts', contactsRouter);
+app.use('/', require('./routes'));
 
 // server starts when db is connected
 connectDb().then(() => {

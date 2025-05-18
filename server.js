15e52d8368fc3bcba225e79972e7149ab7b0ba60
schmitty4437 <schmitty4437@gmail.com>
2025-05-17 20:16:51 -0600

    ww04fixes

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { connectDb } = require('./db/connection');
 const nameRouter = require('./routes/index');
-const contactsRouter = require('./routes/contacts');
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//const contactsRouter = require('./routes/contacts');
 
 // Enable CORS. Allows requests from any origin. Allows resources on my server to be requested.
 //allow all origins, allow post/get, allow content-type
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
@@ -26,7 +24,9 @@ app.use(express.json());
 
 app.use('/', nameRouter);
 // sends requests with /contacts to routes/contacts.js
-app.use('/contacts', contactsRouter);
+//app.use('/contacts', contactsRouter);
+
+app.use('/', require('./routes'));
 
 // server starts when db is connected
 connectDb().then(() => {
